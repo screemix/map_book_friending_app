e95402b6d1db59d3193e96038b72ee831fc919a9
pakrentos <n.smirnov@innopolis.university>
2021-11-08 12:26:04 +0300

    Added connection to database

---
+++ app/internal/database.py
@@ -0,0 +1,17 @@
+import motor.motor_asyncio
+from fastapi_users.db import MongoDBUserDatabase
+
+from app.internal.users.models import UserDB
+
+file = open('database_url')
+DATABASE_URL = file.readline()
+file.close()
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    DATABASE_URL, uuidRepresentation="standard"
+)
+db = client["test"]
+collection = db["users"]
+
+
+async def get_user_db():
+    yield MongoDBUserDatabase(UserDB, collection)
