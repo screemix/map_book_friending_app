5b9d6b563cdb9471118375cc30630b5eddf43f2a
pakrentos <n.smirnov@innopolis.university>
2021-11-16 20:18:35 +0300

    Added books search support

--- backend/app/internal/database.py
+++ backend/app/internal/database.py
@@ -1,17 +1,24 @@
 import motor.motor_asyncio
 from fastapi_users.db import MongoDBUserDatabase
 
-from app.internal.users.models import UserDB
+from .books.database import MongoDBBookDatabase
+from .users.models import UserDB
+from .books.models import BookDB
 
 file = open('database_url')
-DATABASE_URL = file.readline()
+DATABASE_URL = file.readline().strip()
 file.close()
 client = motor.motor_asyncio.AsyncIOMotorClient(
     DATABASE_URL, uuidRepresentation="standard"
 )
 db = client["test"]
-collection = db["users"]
+user_collection = db["users"]
+book_collection = db["books"]
 
 
 async def get_user_db():
-    yield MongoDBUserDatabase(UserDB, collection)
+    yield MongoDBUserDatabase(UserDB, user_collection)
+
+
+async def get_book_db():
+    yield MongoDBBookDatabase(BookDB, book_collection)
